f5786ad66a411fa249ed94e9922127519ddb19b2
Jhoandry Sequera <jsequera826@gmail.com>
2021-05-04 22:29:28 -0300

    clean img/ change colors and logo image

--- src/component/header/index.js
+++ src/component/header/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./index.css"
 import { Navbar, Nav } from 'react-bootstrap'
-import logo from "../../img/logo-512-white.png"
+import logo from "../../img/logo.png"
 
 const Header = () => {
     return (
